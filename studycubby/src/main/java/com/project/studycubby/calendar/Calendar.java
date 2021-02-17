0f4066f0c2ccfccc57502e4189524a08988e6d21
FadingEcho20 <54212394+FadingEcho20@users.noreply.github.com>
2021-02-17 00:09:09 -0600

    inserted expected methods
    created empty methods to act as placeholders in calendar.java

--- studycubby/src/main/java/com/project/studycubby/calendar/Calendar.java
+++ studycubby/src/main/java/com/project/studycubby/calendar/Calendar.java
@@ -3,5 +3,27 @@ import java.util.*;
 
 public class Calendar
 {
+    /**
+     * creates an entry to the calendar database with new id
+     */
+    public void addEntry()
+    {
 
+    }
+
+    /**
+     * deletes entry with object id: id
+     */
+    public void deleteEntry()
+    {
+
+    }
+
+    /**
+     * overwrites entry with object id: id
+     */
+    public void editEntry()
+    {
+
+    }
 }
