960e311a61faf24c05a539b05c2363c2897272ed
narekob1998 <71529218+narekob1998@users.noreply.github.com>
2021-02-17 20:35:21 -0600

    Flashcards

---
+++ studycubby/src/main/java/com/project/studycubby/flashcards/FlashStart.java
@@ -0,0 +1,14 @@
+package com.project.studycubby.flashcards;
+
+public interface FlashStart
+{
+    public default String getSubjectName() {
+
+        return  "";
+    }
+
+    public default String getSubjectNumber() {
+
+        return "";
+    }
+}
