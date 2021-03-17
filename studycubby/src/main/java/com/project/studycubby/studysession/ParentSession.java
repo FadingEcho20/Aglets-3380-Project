545c06e3616e8150935da100af384c61df7bf458
jaymeeaton02 <77986526+jaymeeaton02@users.noreply.github.com>
2021-03-16 23:54:25 -0500

    Add files via upload
    This is the updated parent session with a working timer that is not displayed but counts down from the specified time in seconds. The startSession and endSession functions need to be updated with functions to switch pages.

--- studycubby/src/main/java/com/project/studycubby/studysession/ParentSession.java
+++ studycubby/src/main/java/com/project/studycubby/studysession/ParentSession.java
@@ -1,41 +1,85 @@
 package com.project.studycubby.studysession;
 
 import java.util.*;
+import java.io.*;
 
-public class ParentSession {
+//ParentSession initializes the timer, the startSession function and the endSession function in order to send users to a study session for a specified period of time.
 
+public class ParentSession 
+{
+    static Timer timer;
     protected int sessionLength;
     protected int breakAmount;
-    protected int breakLength;
-    protected ArrayList<String> subjects = new ArrayList<>();
     protected int blockLength;
-
-    protected void timer() {
-        //a timer will be implemented here
-    }
-
-    public void startSession() {
-        //timer will count down from blockLength time (minutes) with breakAmount number of breaks. After each break another blockLength timer will count down. When there are no more breaks, call endSession.
-    }
-
-    public void endSession() {
-        //ends study session
-    }
-
-    public void setSubjects() {
-        switch(breakAmount) {
-            case 1:
-                //user inputs 2 subjects
-                break;
-            case 2:
-                //user inputs 3 subjects
-                break;
-            case 3: 
-                //user inputs 4 subjects
-                break;
-            default:
-                //user inputs 1 subject
-        }
+    
+    public ParentSession(int seconds)
+    {
+        timer = new Timer();
+        timer.schedule(new Run(), seconds*1000);
     }
     
+    public class Run extends TimerTask
+    {
+        public void run()
+        {
+            timer.cancel();
+        }
+    }
+
+    public void startSession() 
+    {
+        //timer will count down from blockLength time (minutes) with breakAmount number of breaks. After each break another blockLength timer will count down. When there are no more breaks, call one more Session and then endSession.
+        sessionLength = breakAmount + 1;
+        
+        if(sessionLength != 0)
+        {
+            System.out.println("This session has started!");
+            
+            while(breakAmount != 0)
+            {
+                for(int i = 0; i <= breakAmount; i++)
+                {
+                    System.out.println("Time to work.");
+                    
+                    //1800 seconds is equal to 30 minutes
+                    new ParentSession(1800);
+                    
+                    System.out.println("Good work!");
+                    System.out.println("Time for a break!");
+                    System.out.println("You have 5 minutes.");
+                        
+                    //300 seconds is equal to 5 minutes
+                    new ParentSession(300);
+                    
+                    System.out.println("The break is over");
+                    
+                    i++;
+                    sessionLength--;
+                }
+            }
+            if(breakAmount == 0)
+            {
+                System.out.println("Time to work.");
+                    
+                new ParentSession(1800);
+                    
+                System.out.println("Good work!");
+                
+                sessionLength--;
+            }
+        }
+        else
+        {
+            endSession();
+        }
+    }
+
+    public void endSession() 
+    {
+        //ends study session
+        if(sessionLength == 0)
+        {
+            System.out.println("This session has ended.");
+        }
+    }
 }
