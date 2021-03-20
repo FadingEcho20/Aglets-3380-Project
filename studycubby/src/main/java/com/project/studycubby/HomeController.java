3e1fa20d36321702635d8845cb4a2e997d8e31bd
stephaniemay <77986566+stephaniemay@users.noreply.github.com>
2021-03-20 00:36:20 -0500

    Add files via upload

--- studycubby/src/main/java/com/project/studycubby/HomeController.java
+++ studycubby/src/main/java/com/project/studycubby/HomeController.java
@@ -4,30 +4,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class HomeController {
+public class HomeController 
+{
 
 	@RequestMapping("/")
-	public String home() {
+	public String home() 
+        {
 		return "Home Page example";
 	}
 
 	@RequestMapping("/Home Page example")
-	public String returnHome() {
+	public String returnHome() 
+        {
 		return "Home Page example";
 	}
 
 	@RequestMapping("/SessionTypeSelection")
-	public String sessionTypeSelection() {
+	public String sessionTypeSelection() 
+        {
 		return "SessionTypeSelection";
 	}
 
 	@RequestMapping("/CalendarPage")
-	public String calendar() {
+	public String calendar() 
+        {
 		return "CalendarPage";
 	}
 
 	@RequestMapping("/Notespage")
-	public String notes() {
+	public String notes() 
+        {
 		return "Notespage";
 	}
 
